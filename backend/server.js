490dea514ef8f88d8cccd255c5ebca9b6769c588
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 17:38:01 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,12 @@
 const express = require('express');
-const path = require('path');
 
 const app = express();
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, '../frontend')));
-
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.send('Franz Finance ONLINE');
 });
 
-app.listen(3000, () => {
-  console.log('Sistema Franz rodando na porta 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log('Servidor rodando na porta', PORT);
 });
