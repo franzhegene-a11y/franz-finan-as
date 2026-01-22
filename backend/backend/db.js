70944586f6e342f1cd0eb504f037bf4309a713d5
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 16:52:42 -0300

    Create db.js

---
+++ backend/backend/db.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+
+module.exports = mysql.createConnection({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE
+});
