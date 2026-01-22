ca191d945175331909d43b246fa24b4e829b4fb9
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 17:30:30 -0300

    Delete backend directory

--- backend/backend/db.js
+++
@@ -1,8 +0,0 @@
-const mysql = require('mysql2');
-
-module.exports = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
-});
