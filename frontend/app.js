9da2e05753c63a0c2b44151d44f26425a02ca32f
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 18:40:50 -0300

    Update app.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,1 +1,19 @@
-console.log('Sistema Franz carregado');
+function login() {
+  const username = document.querySelector('input[type=text]').value;
+  const password = document.querySelector('input[type=password]').value;
+
+  fetch('/login', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ username, password })
+  })
+    .then(res => {
+      if (!res.ok) throw new Error();
+      return res.json();
+    })
+    .then(() => {
+      localStorage.setItem('logado', 'sim');
+      window.location.href = 'dashboard.html';
+    })
+    .catch(() => alert('Usuário ou senha inválidos'));
+}
