c5a7671f8575b77112395ff8144333494f8f5850
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 17:15:11 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
+
+// Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
+// Rota principal
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
-app.listen(3000, () => {
-  console.log('Sistema Franz rodando');
-});
+// Porta dinâmica (Railway)
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log(`Sistema Franz rodando na porta ${PORT}`);
+});
