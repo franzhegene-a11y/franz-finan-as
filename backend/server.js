fbe86308195f26e5e9c9e8c7101dccbd095c1490
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 18:39:38 -0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -19,6 +19,30 @@ app.get('/', (req, res) => {
 
 // porta dinâmica do Railway
 const PORT = process.env.PORT || 3000;
+
+app.post('/login', (req, res) => {
+  const { username, password } = req.body;
+
+  db.query(
+    'SELECT * FROM users WHERE username = ?',
+    [username],
+    async (err, results) => {
+      if (err) return res.status(500).json({ error: 'Erro no servidor' });
+      if (results.length === 0)
+        return res.status(401).json({ error: 'Usuário não encontrado' });
+
+      const user = results[0];
+      const valid = await bcrypt.compare(password, user.password);
+
+      if (!valid)
+        return res.status(401).json({ error: 'Senha incorreta' });
+
+      res.json({ success: true });
+    }
+  );
+});
+
+
 app.listen(PORT, () => {
   console.log(`Sistema Franz rodando na porta ${PORT}`);
 });
