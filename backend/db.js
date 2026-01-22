d2c45ac7ea6b38c9cc42003ca698c945bb651a88
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 18:49:16 -0300

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,22 @@
 const mysql = require('mysql2');
 
-module.exports = mysql.createConnection({
+const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
+connection.connect(err => {
+  if (err) {
+    console.error('❌ Erro ao conectar no MySQL:', err);
+  } else {
+    console.log('✅ MySQL conectado com sucesso');
+  }
+});
+
+module.exports = connection;
