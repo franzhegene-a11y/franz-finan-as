5fad1738f8170c009e65174985d73e4d7907afae
franzhegene-a11y <franzhegene@gmail.com>
2026-01-22 16:52:20 -0300

    backend/config.js

---
+++ backend/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  PRICES: {
+    buffet: 33,
+    marmita_p: 20,
+    marmita_m: 22,
+    marmita_g: 26,
+    empresa: 22
+  },
+  FIXED_COST_MONTHLY: 10470,
+  DAYS_OPEN: 26
+};
